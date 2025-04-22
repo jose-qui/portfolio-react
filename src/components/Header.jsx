2e1587febc4cee77fe97bdf839f2104b270853ce
jose-qui <josequirogaa01@gmail.com>
2025-04-21 22:17:33 -0300

    feat: Se realizan las primeras estructuras

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/App.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="/tu-foto.jpg" alt="Jose Quiroga" className="profile-pic" />
+      <h1>Jose María Quiroga</h1>
+      <h3>Estudiante de Programación (61033)</h3>
+    </header>
+  );
+};
+
+export default Header;
